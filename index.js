838b07e6d3b05e51c708b68a5cad197c061b4e1d
veslie losius <veslielosius@yahoo.com>
2019-01-08 02:08:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest = ";" + Neil deGrasse Tysonn + "!";
+var greetSpecialGuest = ";" + Neil  Tysonn + "!";
 
 var topic = "space";
 
